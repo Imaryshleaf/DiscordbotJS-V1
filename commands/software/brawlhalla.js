9fcedc109ec6b470a4b5390ffbd25e4bdfd09e85
Axvia <65358975+Axvia@users.noreply.github.com>
2021-02-16 09:22:13 +0800

    update the link

--- commands/software/brawlhalla.js
+++ commands/software/brawlhalla.js
@@ -3,10 +3,10 @@ module.exports = async(client, message) => {
     const embedSoftware = new Discord.MessageEmbed()
     .setColor('#62fcd4')
     .setTitle('BNC Software')
-    .setDescription('Brawlhalla - Combo Simplified - by NC - Version 0.2')
+    .setDescription('Brawlhalla - Combo Simplified - by NC - Version 0.3')
     .addField('Information', 
-        `📥 | [Download](https://cdn.discordapp.com/attachments/806504563052904448/807095279974744084/Brawlhalla_-_Combo_Simplified_-_by_NC_-_Version_0.2.rar)\n`+
-        `ℹ️ | Version : \`0.2\`\n`+
+        `📥 | [Download](https://cdn.discordapp.com/attachments/806504563052904448/811024522711007242/Brawlhalla_-_Combo_Simplified_-_by_NC_-_Version_0.3.rar)\n`+
+        `ℹ️ | Version : \`0.3\`\n`+
         `⚙️ | Created by <@&806521838854733844>\n`+
         `💻 | Tested on \`Windows 8\``
     )
